7aa63b6782628e5345bd523024e136fee207ddf1
Greg Wang <gregwym@gmail.com>
2013-01-15 09:59:53 -0500

    Hello World

---
+++ src/ca/uwaterloo/joos/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ca.uwaterloo.joos;
+
+/**
+ * @author Greg Wang
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.printf("Hello World!");
+	}
+
+}
