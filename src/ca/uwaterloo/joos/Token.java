a2534228d4d3660cca887939881dfb452d00383e
Greg Wang <gregwym@gmail.com>
2013-01-18 21:23:38 -0800

    Implement Scanner which use DFA to scan input into Token[s]

---
+++ src/ca/uwaterloo/joos/Token.java
@@ -0,0 +1,17 @@
+package ca.uwaterloo.joos;
+
+//import java.util.*;
+
+public class Token {
+	String kind;
+	String lexeme;
+
+	public Token(String kind, String lexeme) {
+		this.kind = kind;
+		this.lexeme = lexeme;
+	}
+	
+	public String toString() {
+		return this.kind + " " + this.lexeme;
+	}
+}
