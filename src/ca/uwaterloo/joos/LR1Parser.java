fa82dd1f394bdb1136d485436135032bb2321a33
Greg Wang <gregwym@gmail.com>
2013-02-07 16:10:24 -0500

    Add comments for LR1Parser

--- src/ca/uwaterloo/joos/LR1Parser.java
+++ src/ca/uwaterloo/joos/LR1Parser.java
@@ -39,47 +39,63 @@ public class LR1Parser {
 	}
 
 	public boolean checkGrammer(List<Token> tokens) {
+		// Initialize startSymbol, tokens iterator and parsing stack
 		String startSymbol = lr1.getStartSymbol();
 		ListIterator<Token> tokensIterator = tokens.listIterator();
 		Stack<TransitionState> parseStack = new Stack<TransitionState>();
 		parseStack.push(new TransitionState(null, 0));
 
-		int currentState = 0;
+		// Initialize parsing state variables
 		Token nextToken = tokensIterator.next();
 		Action nextAction = null;
+		
+		// Execute lr1 parsing until the tokens has been reduced to the startSymbol
 		while (!nextToken.getKind().equals(startSymbol)) {
-			currentState = parseStack.peek().getState();
-			nextAction = lr1.actionFor(currentState, nextToken.getKind());
-			LR1Parser.logger.fine("Next:\tState " + currentState + " + " + nextToken + " => " + nextAction);
+			
+			// Acquire next action, according to the current state and the next token kind
+			nextAction = lr1.actionFor(parseStack.peek().getState(), nextToken.getKind());
+			LR1Parser.logger.fine("Next:\tState " + parseStack.peek().getState() + " + " + nextToken + " => " + nextAction);
 
+			// If is a shift
 			if (nextAction instanceof LR1.ActionShift) {
 				ActionShift shift = (ActionShift) nextAction;
 				LR1Parser.logger.info("Shift:\t" + shift);
 
+				// Push the transitionState to the stack
 				TransitionState transitionState = new TransitionState(nextToken, shift.getToState());
 				parseStack.push(transitionState);
 				LR1Parser.logger.info("├─ Push:\t" + transitionState);
 
+				// Iterate to next token
+				// Note: if there is no next, move cursor back to front first
 				if(!tokensIterator.hasNext()) tokensIterator = tokens.listIterator();
 				nextToken = tokensIterator.next();
 			}
+			// If is a reduction
 			else if (nextAction instanceof LR1.ActionReduce){
 				ActionReduce reduce = (ActionReduce) nextAction;
 				LR1Parser.logger.info("Redue:\t" + reduce);
 
+				// Pop the stack according to the length of the production rule's RHS
 				for (int i = 0; i < reduce.getInt(); i++) {
 					TransitionState poppedState = parseStack.pop();
 					LR1Parser.logger.info("├─ Pop:\t" + poppedState);
 				}
+				
+				// Iterate back one token, since it has not been pushed to the stack
 				tokensIterator.previous();
+				
+				// Next token is the production rule's LHS
 				nextToken = new Token(reduce.getProductionRule().getLefthand(), Arrays.toString(reduce.getProductionRule().getRighthand()));
 			}
+			// No action found, validation failed
 			else {
-				LR1Parser.logger.severe("ERROR: No valid action for State " + currentState + " + " + nextToken);
+				LR1Parser.logger.severe("ERROR: No valid action for State " + parseStack.peek().getState() + " + " + nextToken);
 				return false;
 			}
 		}
 
+		// The tokens has been reduced to startSymbol, validation succeed
 		LR1Parser.logger.info("Tokens has been validated against LR1");
 		return true;
 	}
