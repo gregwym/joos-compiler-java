65a495b324ae24fa18114f0f892c1a7cec8fe97c
wenzhuman <manwenzhu@gmail.com>
2013-02-13 12:58:24 -0500

    modifiy A1Test for testing

--- src/ca/uwaterloo/joos/test/A1Test.java
+++ src/ca/uwaterloo/joos/test/A1Test.java
@@ -2,10 +2,15 @@ package ca.uwaterloo.joos.test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.logging.Level;
-
+//import java.util.regex.Pattern;
+//import java.util.regex.Matcher;
 import org.junit.Test;
 
 import ca.uwaterloo.joos.Main;
@@ -23,13 +28,19 @@ public class A1Test {
 	//public static void main(String[] args) {
 	public void Test() 
 	{
+		try {
+			System.setOut(new PrintStream(new File("output.txt")));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		//fail("Not yet implemented");
 		File directory = new File("resources/testcases/a1");
 		File[] testFileList = directory.listFiles();
 			for (File testFile : testFileList)
 			{
 				
-				System.out.println(testFile.getName());
+				System.out.println("currentFile"+testFile.getName());
 				
 			
 				Main.getLogger().setLevel(Level.INFO);
@@ -77,7 +88,7 @@ public class A1Test {
 	
 				try 
 				{
-				    lr1 = new LR1(new File("resources/sample.lr1"));
+				    lr1 = new LR1(new File("resources/joos.lr1"));
 				} 
 				catch (Exception e) 
 				{
@@ -101,6 +112,27 @@ public class A1Test {
 	
 				System.out.println(parseTree);
 			}
+	}
+	public void extractError(File testFile)
+	{
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(testFile));
+			String line = br.readLine();
+			if(line.contains("JOO1:"))
+			{
+				if(line.contains("PARSER_EXCEPTION"))
+				{
+					
+				}
+				else if(line.contains("PARSER_EXCEPTION"))
+				{
+					
+				}
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 	}
 }
 
