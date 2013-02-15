56a906c89f45172b4dd280d7b2d7690874818292
wenzhuman <manwenzhu@gmail.com>
2013-02-15 11:32:19 -0500

    add integer range check in preprocess

--- src/ca/uwaterloo/joos/Preprocessor.java
+++ src/ca/uwaterloo/joos/Preprocessor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import ca.uwaterloo.joos.scanner.Token;
+import ca.uwaterloo.joos.scanner.Scanner.ScanException;
 
 public class Preprocessor {
 
@@ -17,15 +18,38 @@ public class Preprocessor {
 		this.undesiredTokenKinds.add("JAVADOC");
 	}
 
-	public List<Token> processTokens(List<Token> rawTokens) {
+	public List<Token> processTokens(List<Token> rawTokens) throws ScanException {
 		List<Token> tokens = new ArrayList<Token>();
 
 		for(Token token: rawTokens) {
 			if(!this.undesiredTokenKinds.contains(token.getKind())) tokens.add(token);
+			if(token.getKind().equals("INTLIT"))
+			{
+				checkIntRange(token.getLexeme());
+			}
 		}
 
 		tokens.add(0, new Token("BOF","BOF"));
 		tokens.add(new Token("EOF","EOF"));
 		return tokens;
+	}
+	
+	private static void checkIntRange(String intString)throws ScanException{
+		String intergerThreshold = "2147483647";
+		
+		if(intString.length() == intergerThreshold.length())
+		{
+		   for(int i = 0;i<intergerThreshold.length();i++)
+		   {
+			   if((int)intString.charAt(i)>(int)intergerThreshold.charAt(i)){
+				  
+					throw new ScanException("Interger out of Range");
+			   }
+		   }
+		}
+		if(intString.length() > intergerThreshold.length())
+		{
+			throw new ScanException("Interger out of Range");	
+		}
 	}
 }
