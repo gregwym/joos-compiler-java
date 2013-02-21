9a38ec856fd9b0dd533a480e48e79d64098f9e77
wenzhuman <manwenzhu@gmail.com>
2013-02-21 17:40:10 -0500

    different type of imports

---
+++ src/ca/uwaterloo/joos/ast/decl/SingleImport.java
@@ -0,0 +1,13 @@
+package ca.uwaterloo.joos.ast.decl;
+
+import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.parser.ParseTree.Node;
+
+public class SingleImport extends ImportDeclaration {
+
+	public SingleImport(Node node, ASTNode parent) throws Exception {
+		super(node, parent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
