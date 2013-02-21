b6561cd910c156a0105bd0f75e129f3b49161c93
wenzhuman <manwenzhu@gmail.com>
2013-02-21 16:00:30 -0500

    add packagedeclaration and importdeclaration

--- src/ca/uwaterloo/joos/ast/decl/ImportDeclaration.java
+++ src/ca/uwaterloo/joos/ast/decl/ImportDeclaration.java
@@ -1,27 +1,36 @@
 package ca.uwaterloo.joos.ast.decl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.ast.descriptor.ChildDescriptor;
+import ca.uwaterloo.joos.ast.name.QualifiedName;
 import ca.uwaterloo.joos.parser.ParseTree.LeafNode;
 import ca.uwaterloo.joos.parser.ParseTree.Node;
 import ca.uwaterloo.joos.parser.ParseTree.TreeNode;
 
-public class ImportDeclaration extends ASTNode{
-	
+public class ImportDeclaration extends ASTNode {
+	public static final ChildDescriptor IMPORTNAME = new ChildDescriptor(QualifiedName.class);
+
 	public ImportDeclaration(Node node, ASTNode parent) throws Exception {
 		super(node, parent);
 	}
 
 	@Override
 	public List<Node> processTreeNode(TreeNode treeNode) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Node> offers = new ArrayList<Node>();
+		if (treeNode.productionRule.getLefthand().equals("importdecls")) {
+			QualifiedName qualifiedName = new QualifiedName(treeNode, this);
+			addChild(IMPORTNAME, qualifiedName);
+		} else {
+			offers.addAll(treeNode.children);
+		}
+		return offers;
 	}
 
 	@Override
 	public void processLeafNode(LeafNode leafNode) throws Exception {
-		// TODO Auto-generated method stub
-		
+
 	}
 }
