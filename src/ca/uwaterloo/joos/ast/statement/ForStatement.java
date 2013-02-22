908fc6528fdf42cefcb5bb21c3116809f0b2aa50
wenzhuman <manwenzhu@gmail.com>
2013-02-22 17:00:56 -0500

    add for statement

---
+++ src/ca/uwaterloo/joos/ast/statement/ForStatement.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ca.uwaterloo.joos.ast.statement;
+
+import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.parser.ParseTree.Node;
+
+/**
+ * @author wenzhuman
+ *
+ */
+public class ForStatement extends Statement {
+
+	/**
+	 * @param node
+	 * @param parent
+	 * @throws Exception
+	 */
+	public ForStatement(Node node, ASTNode parent) throws Exception {
+		super(node, parent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
