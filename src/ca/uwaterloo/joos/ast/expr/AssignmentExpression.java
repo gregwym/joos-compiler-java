ebd63e928bd55c7dfdf6a86bea711b42463631ec
Greg Wang <gregwym@gmail.com>
2013-02-22 14:47:23 -0500

    Implement part AssignmentExpr and MethodInvokeExpr

--- src/ca/uwaterloo/joos/ast/expr/AssignmentExpression.java
+++ src/ca/uwaterloo/joos/ast/expr/AssignmentExpression.java
@@ -1,13 +1,46 @@
 package ca.uwaterloo.joos.ast.expr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.ast.descriptor.ChildDescriptor;
+import ca.uwaterloo.joos.parser.ParseTree.LeafNode;
 import ca.uwaterloo.joos.parser.ParseTree.Node;
+import ca.uwaterloo.joos.parser.ParseTree.TreeNode;
 
 public class AssignmentExpression extends Expression {
-
+	
+//	public static final ChildDescriptor LEFTHAND = new ChildDescriptor(Lefthand.class);
+	public static final ChildDescriptor EXPR = new ChildDescriptor(Expression.class);
+	
 	public AssignmentExpression(Node node, ASTNode parent) throws Exception {
 		super(node, parent);
-		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public List<Node> processTreeNode(TreeNode treeNode) throws Exception {
+		if (treeNode.getKind().equals("lefthand")) {
+//			Name name = new SimpleName(treeNode, this);
+//			this.addChild(NAME, name);
+		} else if (treeNode.getKind().equals("assignop")) {
+//			Name name = new QualifiedName(treeNode, this);
+//			this.addChild(NAME, name);
+		} else if (treeNode.getKind().equals("assignexpr")) {
+			// TODO Turn of after expr finished
+//			Expression expr = Expression.newExpression(treeNode, this);
+//			this.addChild(EXPR, expr);
+		} else {
+			List<Node> offers = new ArrayList<Node>();
+			offers.addAll(treeNode.children);
+			return offers;
+		}
+		return null;
+	}
+
+	@Override
+	public void processLeafNode(LeafNode leafNode) throws Exception {
+
 	}
 
 }
--- src/ca/uwaterloo/joos/ast/expr/MethodInvokeExpression.java
+++ src/ca/uwaterloo/joos/ast/expr/MethodInvokeExpression.java
@@ -1,13 +1,50 @@
 package ca.uwaterloo.joos.ast.expr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.ast.descriptor.ChildDescriptor;
+import ca.uwaterloo.joos.ast.name.Name;
+import ca.uwaterloo.joos.ast.name.QualifiedName;
+import ca.uwaterloo.joos.ast.name.SimpleName;
+import ca.uwaterloo.joos.parser.ParseTree.LeafNode;
 import ca.uwaterloo.joos.parser.ParseTree.Node;
+import ca.uwaterloo.joos.parser.ParseTree.TreeNode;
 
 public class MethodInvokeExpression extends Expression {
 
+	public static final ChildDescriptor NAME = new ChildDescriptor(Name.class);
+	public static final ChildDescriptor PRIMARY = new ChildDescriptor(Primary.class);
+
 	public MethodInvokeExpression(Node node, ASTNode parent) throws Exception {
 		super(node, parent);
-		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Node> processTreeNode(TreeNode treeNode) throws Exception {
+		if (treeNode.getKind().equals("simplename")) {
+			Name name = new SimpleName(treeNode, this);
+			this.addChild(NAME, name);
+		} else if (treeNode.getKind().equals("qualifiedname")) {
+			Name name = new QualifiedName(treeNode, this);
+			this.addChild(NAME, name);
+		} else if (treeNode.getKind().equals("primary")) {
+			Primary primary = Primary.newPrimary(treeNode, this);
+			this.addChild(PRIMARY, primary);
+		} else if (treeNode.getKind().equals("methodinvoke")) {
+
+		} else {
+			List<Node> offers = new ArrayList<Node>();
+			offers.addAll(treeNode.children);
+			return offers;
+		}
+		return null;
+	}
+
+	@Override
+	public void processLeafNode(LeafNode leafNode) throws Exception {
+
 	}
 
 }
