994d88ddc206453dba119c4ec4173499db07e66d
Greg Wang <gregwym@gmail.com>
2013-03-02 12:14:00 -0500

    Add stdlib

---
+++ stdlib/4.0/java/lang/Byte.java
@@ -0,0 +1,10 @@
+package java.lang;
+public final class Byte {
+    public byte value;
+    public Byte(byte i) {
+        value = i;
+    }
+    public Byte() {
+    }
+    public static byte MAX_VALUE = (byte)127;
+}
