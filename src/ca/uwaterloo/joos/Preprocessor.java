d8e14b6e700f26d72695cc6bb0997f1c0d0f8ee0
wenzhuman <manwenzhu@gmail.com>
2013-03-03 21:17:41 -0500

    move the integer checker to weede

--- src/ca/uwaterloo/joos/Preprocessor.java
+++ src/ca/uwaterloo/joos/Preprocessor.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import ca.uwaterloo.joos.scanner.Token;
-import ca.uwaterloo.joos.scanner.Scanner.ScanException;
+
 
 public class Preprocessor {
 
@@ -18,54 +17,15 @@ public class Preprocessor {
 		this.undesiredTokenKinds.add("JAVADOC");
 	}
 
-	public List<Token> processTokens(List<Token> rawTokens) throws ScanException {
+	public List<Token> processTokens(List<Token> rawTokens) {
 		List<Token> tokens = new ArrayList<Token>();
 		
 		for(Token token:rawTokens){
 			if(!this.undesiredTokenKinds.contains(token.getKind())) tokens.add(token);
 			
 		}
-		for(int i = 0;i<tokens.size();i++){
-			Token token = tokens.get(i);
-			if(token.getKind().equals("INTLIT"))
-			{ 
-				String intPositiveThreshold = "2147483647";
-				String intNegThreshold = "2147483648";
-				Set<String> expressionTokenKinds = new HashSet<String>();
-				expressionTokenKinds.add("INTLIT");
-				expressionTokenKinds.add("RPAREN");
-				if(tokens.get(i-1).getKind().equals("MINUS")&&!expressionTokenKinds.contains(tokens.get(i-2).getKind()))
-				{
-					checkIntRange(token.getLexeme(),intNegThreshold);
-				}
-				else
-			   {
-				checkIntRange(token.getLexeme(),intPositiveThreshold);
-				}
-			}
-		}
-
 		tokens.add(0, new Token("BOF","BOF"));
 		tokens.add(new Token("EOF","EOF"));
 		return tokens;
-	}
-	
-	private static void checkIntRange(String intString,String intergerThreshold)throws ScanException{
-		
-		
-		if(intString.length() == intergerThreshold.length())
-		{
-		   for(int i = 0;i<intergerThreshold.length();i++)
-		   {
-			   if((int)intString.charAt(i)>(int)intergerThreshold.charAt(i)){
-				  
-					throw new ScanException("Interger out of Range");
-			   }
-		   }
-		}
-		if(intString.length() > intergerThreshold.length())
-		{  
-			throw new ScanException("Interger out of Range");	
-		}
 	}
 }
---
+++ src/ca/uwaterloo/joos/weeder/IntegerChecker.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package ca.uwaterloo.joos.weeder;
+
+import java.util.HashSet;
+import java.util.Set;
+import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.ast.expr.primary.LiteralPrimary;
+import ca.uwaterloo.joos.ast.visitor.IntegerVisitor;
+import ca.uwaterloo.joos.scanner.Scanner.ScanException;
+
+/**
+ * @author wenzhuman
+ * 
+ */
+public class IntegerChecker extends IntegerVisitor {
+
+	/**
+	 * @throws Exception
+	 * 
+	 */
+	@Override
+	public void willVisit(ASTNode node) {
+
+	}
+
+	@Override
+	public void didVisit(ASTNode node) {
+
+	}
+
+	@Override
+	protected void visitInteger(LiteralPrimary node) throws Exception {
+		String intPositiveThreshold = "2147483647";
+		String intNegThreshold = "2147483648";
+		Set<String> expressionTokenKinds = new HashSet<String>();
+		expressionTokenKinds.add("INTLIT");
+		expressionTokenKinds.add("RPAREN");
+		if(node.getParent().getClass().getSimpleName().equals("UnaryExpression"))
+		{
+			checkIntRange(node.getValue(),intNegThreshold);
+		}
+		else
+	   {
+			checkIntRange(node.getValue(),intPositiveThreshold);
+		}
+		
+	}
+	private void checkIntRange(String intString,String intergerThreshold)throws Exception{
+		
+		
+		if(intString.length() == intergerThreshold.length())
+		{
+		   for(int i = 0;i<intergerThreshold.length();i++)
+		   {
+			   if((int)intString.charAt(i)>(int)intergerThreshold.charAt(i)){
+				  
+					throw new Exception("Interger out of Range");
+			   }
+		   }
+		}
+		if(intString.length() > intergerThreshold.length())
+		{  
+			throw new Exception("Interger out of Range");	
+		}
+	}
+}
