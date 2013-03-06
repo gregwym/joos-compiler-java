ab6579ee37d829f551f94180aa8970a17fd1926e
Greg Wang <gregwym@gmail.com>
2013-03-06 03:08:55 -0500

    Revert testcases/a1/*

--- testcases/a1/J1_01.java
+++ testcases/a1/J1_01.java
@@ -1,12 +1,6 @@
 // PARSER_WEEDER,CODE_GENERATION
-package ca.test;
-
-import J1_char.ca;
-
-public class J1_01 implements J1_char {
-	public int field;
+public class J1_01 {
     public J1_01() {}
-
     public static int test() {
 	int r1 = J1_01.m0(0);
 	int r2 = J1_01.m0(1);
@@ -14,12 +8,12 @@ public class J1_01 implements J1_char {
 	int r4 = J1_01.m1(0);
 	int r5 = J1_01.m1(1);
 	int r6 = J1_01.m1(100);
+
+	int r = 0;
 	if (r1==9) r=r+1;
-	{
-		int r1;
-	}
 	if (r2==6) r=r+1;
 	if (r3==6) r=r+1;
+
 	if (r4==6) r=r+1;
 	if (r5==9) r=r+1;
 	if (r6==6) r=r+1;
