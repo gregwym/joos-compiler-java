c779e4e259b9f9598bc4bf9bc6d4184601f94841
wenzhuman <manwenzhu@gmail.com>
2013-03-06 12:51:11 -0500

    add implements for classdecl

--- src/ca/uwaterloo/joos/ast/decl/ClassDeclaration.java
+++ src/ca/uwaterloo/joos/ast/decl/ClassDeclaration.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import ca.uwaterloo.joos.ast.ASTNode;
 import ca.uwaterloo.joos.ast.descriptor.ChildDescriptor;
+import ca.uwaterloo.joos.ast.descriptor.ChildListDescriptor;
 import ca.uwaterloo.joos.ast.type.ReferenceType;
 import ca.uwaterloo.joos.parser.ParseTree.Node;
 import ca.uwaterloo.joos.parser.ParseTree.TreeNode;
@@ -11,6 +12,7 @@ import ca.uwaterloo.joos.parser.ParseTree.TreeNode;
 public class ClassDeclaration extends TypeDeclaration {
 
 	protected static final ChildDescriptor SUPER = new ChildDescriptor(ReferenceType.class);
+	
 
 	public ClassDeclaration(Node node, ASTNode parent) throws Exception {
 		super(node, parent);
@@ -18,17 +20,21 @@ public class ClassDeclaration extends TypeDeclaration {
 
 	/**
 	 * @return the superClass
-	 * @throws ChildTypeUnmatchException 
+	 * @throws ChildTypeUnmatchException
 	 */
 	public ReferenceType getSuperClass() throws ChildTypeUnmatchException {
 		return (ReferenceType) this.getChildByDescriptor(ClassDeclaration.SUPER);
 	}
-	
+
 	@Override
 	public List<Node> processTreeNode(TreeNode treeNode) throws Exception {
-		if (treeNode.productionRule.getLefthand().equals("super")) {
+		if (treeNode.productionRule.getLefthand().equals("super")&&(treeNode.children.size()!=0)) {
 			ReferenceType superType = new ReferenceType(treeNode, this);
 			addChild(SUPER, superType);
+		}
+		else if(treeNode.productionRule.getLefthand().equals("interfaces")&&(treeNode.children.size()!=0)){
+			ReferenceType implementType = new ReferenceType(treeNode, this);
+			addChild(IMPLEMNTS, implementType);
 		}
 		else {
 			return super.processTreeNode(treeNode);
