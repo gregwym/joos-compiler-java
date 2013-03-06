5a2843b654102a2dcd86e1e4aec878ed23f3be49
wenzhuman <manwenzhu@gmail.com>
2013-03-06 10:20:22 -0500

    deleted auto-comment  Please enter the commit message for your changes. Lines starting

--- src/ca/uwaterloo/joos/ast/visitor/MethodDeclVisitor.java
+++ src/ca/uwaterloo/joos/ast/visitor/MethodDeclVisitor.java
@@ -2,31 +2,29 @@ package ca.uwaterloo.joos.ast.visitor;
 
 import ca.uwaterloo.joos.ast.ASTNode;
 import ca.uwaterloo.joos.ast.decl.MethodDeclaration;
-import ca.uwaterloo.joos.ast.expr.primary.LiteralPrimary;
 
 public abstract class MethodDeclVisitor extends ASTVisitor {
 
 	public MethodDeclVisitor() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	@Override
 	public void willVisit(ASTNode node) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void didVisit(ASTNode node) {
-		// TODO Auto-generated method stub
 
 	}
+
 	public boolean visit(ASTNode node) throws Exception {
 
 		if (node instanceof MethodDeclaration) {
 			MethodDeclaration methodDeclNode = (MethodDeclaration) node;
-			
-				this.visitMethodDecl(methodDeclNode);
+
+			this.visitMethodDecl(methodDeclNode);
 		}
 		return true;
 	}
