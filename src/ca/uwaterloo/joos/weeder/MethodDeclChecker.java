c226d24702d7bcfca6495c07033098731ab30e24
wenzhuman <manwenzhu@gmail.com>
2013-03-06 10:17:03 -0500

    add check for class contains abstract methods

---
+++ src/ca/uwaterloo/joos/weeder/MethodDeclChecker.java
@@ -0,0 +1,32 @@
+package ca.uwaterloo.joos.weeder;
+
+import ca.uwaterloo.joos.ast.ASTNode;
+import ca.uwaterloo.joos.ast.Modifiers.Modifier;
+import ca.uwaterloo.joos.ast.decl.ClassDeclaration;
+import ca.uwaterloo.joos.ast.decl.MethodDeclaration;
+import ca.uwaterloo.joos.ast.visitor.MethodDeclVisitor;
+
+public class MethodDeclChecker extends MethodDeclVisitor {
+
+	public MethodDeclChecker() {
+
+	}
+
+	@Override
+	protected void visitMethodDecl(MethodDeclaration node) throws Exception {
+		ASTNode astNode = node;
+		if (node.getModifiers().getModifiers().contains(Modifier.ABSTRACT)) {
+			ClassDeclaration classDeclNode = null;
+			while (!(astNode.getParent() instanceof ClassDeclaration)) {
+				astNode = astNode.getParent();
+			}
+			classDeclNode = (ClassDeclaration) astNode.getParent();
+			if (!(classDeclNode.getModifiers().getModifiers().contains(Modifier.ABSTRACT))) {
+				throw new Exception("class contains abstract Method must be abstract");
+			}
+
+		}
+
+	}
+
+}
