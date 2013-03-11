835a8ee4490a87ce6d3e19fa19ae75a980fc626f
wenzhuman <manwenzhu@gmail.com>
2013-03-11 00:00:51 -0400

    add interface extends

--- src/ca/uwaterloo/joos/checker/HierarchyChecker.java
+++ src/ca/uwaterloo/joos/checker/HierarchyChecker.java
@@ -1,10 +1,10 @@
 package ca.uwaterloo.joos.checker;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class HierarchyChecker {
@@ -21,9 +21,11 @@ public class HierarchyChecker {
 	}
 
 	private void checkImplements() throws Exception {
-		Iterator it = hierarchyBuilder.getImplementMap().entrySet().iterator();
+		Iterator<Entry<String, List<String>>> it = hierarchyBuilder.getImplementMap().entrySet().iterator();
 		while (it.hasNext()) {
+			@SuppressWarnings("rawtypes")
 			Map.Entry pairs = (Map.Entry) it.next();
+			@SuppressWarnings("unchecked")
 			List<String> interfaeces = (List<String>) pairs.getValue();
 			for (String interfaceName : interfaeces) {
 				if (!hierarchyBuilder.getInterfaces().contains(interfaceName)) {
@@ -36,18 +38,16 @@ public class HierarchyChecker {
 
 	private void checkExtends() throws Exception {
 
-		Iterator it = hierarchyBuilder.getHierarchyMap().entrySet().iterator();
+		Iterator<Entry<String, String>> it = hierarchyBuilder.getHierarchyMap().entrySet().iterator();
 		while (it.hasNext()) {
 			Set<String> superClain = new HashSet<String>();
+			@SuppressWarnings("rawtypes")
 			Map.Entry pairs = (Map.Entry) it.next();
-			// System.out.println("adding @@@@" + (String) pairs.getKey());
 			superClain.add((String) pairs.getKey());
 
 			if (superClain.contains((String) pairs.getValue())) {
 				throw new Exception("extend cycle");
 			} else {
-				// System.out.println("adding @@@@" + (String)
-				// pairs.getValue());
 				superClain.add((String) pairs.getValue());
 			}
 			String chain = (String) pairs.getValue();
@@ -56,20 +56,16 @@ public class HierarchyChecker {
 				if (superClain.contains((String) this.hierarchyBuilder.getHierarchyMap().get(chain))) {
 					throw new Exception("extend cycle");
 				} else {
-					// System.out.println("adding @@@@" + (String)
-					// this.hierarchyBuilder.getHierarchyMap().get(chain));
 					superClain.add(this.hierarchyBuilder.getHierarchyMap().get(chain));
 				}
 				chain = this.hierarchyBuilder.getHierarchyMap().get(chain);
 
 			}
-			@SuppressWarnings("rawtypes")
 			String className = (String) pairs.getKey();
-
-			// @SuppressWarnings("unchecked")
 			String SUPER = (String) pairs.getValue();
 
-			if (hierarchyBuilder.getInterfaces().contains(SUPER) && (hierarchyBuilder.getClasses().contains(className))) {
+			if (hierarchyBuilder.getInterfaces().contains(SUPER) && (!hierarchyBuilder.getInterfaces().contains(className))) {
+				System.out.println(hierarchyBuilder.getInterfaces());
 				throw new Exception("a class" + className + " can not extend an interface" + SUPER);
 			}
 			if (hierarchyBuilder.getInterfaces().contains(SUPER) && (hierarchyBuilder.getClasses().contains(SUPER))) {
@@ -79,7 +75,7 @@ public class HierarchyChecker {
 				throw new Exception("can not extend final" + SUPER);
 			}
 
-			it.remove(); // avoids a ConcurrentModificationException
+			it.remove(); 
 		}
 	}
 
