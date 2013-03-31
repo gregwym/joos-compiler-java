64091c2e15f44682b15c264b1994537ee3260fed
Greg Wang <gregwym@gmail.com>
2013-03-30 22:56:31 -0400

    Assign index for locals parameters fields.

--- src/ca/uwaterloo/joos/ast/decl/VariableDeclaration.java
+++ src/ca/uwaterloo/joos/ast/decl/VariableDeclaration.java
@@ -9,14 +9,17 @@ import ca.uwaterloo.joos.parser.ParseTree.Node;
 import ca.uwaterloo.joos.parser.ParseTree.TreeNode;
 
 public abstract class VariableDeclaration extends BodyDeclaration {
-	private int index; //Holds the index of the declaration
 	
 	protected static final ChildDescriptor INITIAL = new ChildDescriptor(Expression.class);
 
 	public VariableDeclaration(Node node, ASTNode parent) throws Exception {
 		super(node, parent);
 	}
-
+	
+	public Expression getInitial() throws ChildTypeUnmatchException{
+		return (Expression) this.getChildByDescriptor(INITIAL);
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -24,20 +27,6 @@ public abstract class VariableDeclaration extends BodyDeclaration {
 	 * ca.uwaterloo.joos.parser.ParseTreeTraverse.Traverser#processTreeNode(
 	 * ca.uwaterloo.joos.parser.ParseTree.TreeNode)
 	 */
-	
-	
-	public void setIndex(int idx){
-		index = idx;
-	}
-	
-	public int getIndex(){
-		return index;
-	}
-	
-	public Expression getInitial() throws ChildTypeUnmatchException{
-		return (Expression) this.getChildByDescriptor(INITIAL);
-	}
-	
 	@Override
 	public List<Node> processTreeNode(TreeNode treeNode) throws Exception {
 		if (treeNode.productionRule.getLefthand().equals("expr")) {
--- src/ca/uwaterloo/joos/codegen/StaticLocalInit.java
+++ src/ca/uwaterloo/joos/codegen/StaticLocalInit.java
@@ -1,92 +1,56 @@
 package ca.uwaterloo.joos.codegen;
-//TODO get the method index
-import java.util.List;
-
 import ca.uwaterloo.joos.ast.ASTNode;
-import ca.uwaterloo.joos.ast.ASTNode.ChildTypeUnmatchException;
-import ca.uwaterloo.joos.ast.FileUnit;
-import ca.uwaterloo.joos.ast.Modifiers;
-import ca.uwaterloo.joos.ast.Modifiers.Modifier;
-import ca.uwaterloo.joos.ast.body.TypeBody;
-import ca.uwaterloo.joos.ast.decl.ClassDeclaration;
 import ca.uwaterloo.joos.ast.decl.FieldDeclaration;
 import ca.uwaterloo.joos.ast.decl.LocalVariableDeclaration;
 import ca.uwaterloo.joos.ast.decl.MethodDeclaration;
-import ca.uwaterloo.joos.ast.decl.PackageDeclaration;
 import ca.uwaterloo.joos.ast.decl.ParameterDeclaration;
-import ca.uwaterloo.joos.symboltable.BlockScope;
+import ca.uwaterloo.joos.ast.decl.TypeDeclaration;
 import ca.uwaterloo.joos.symboltable.SemanticsVisitor;
 import ca.uwaterloo.joos.symboltable.SymbolTable;
-import ca.uwaterloo.joos.symboltable.TableEntry;
-import ca.uwaterloo.joos.symboltable.TypeScope;
 
 public class StaticLocalInit extends SemanticsVisitor {
 
-	protected int locals = 0;//Counts the local variable declarations
-	protected int methods = 0; //Counts the methods
-	private TypeScope ts;
+	protected int parameters = 0;
+	protected int locals = 0;		// Counts the local variable declarations
+	
+	protected int fields = 0;
+	protected int methods = 0;		// Counts the methods
+
 	public StaticLocalInit(SymbolTable table) {
 		super(table);
 	}
 	
-	
 	@Override
 	public void willVisit(ASTNode node) throws Exception{		
+		super.willVisit(node);
 		
-		if (node instanceof TypeBody){
-			//Emit code for all of the static fields
-			
-			List<FieldDeclaration> fields = ((TypeBody)node).getFields();
-			List<MethodDeclaration> methods = ((TypeBody)node).getMethods();
-			for (FieldDeclaration fd : fields){
-				countField(fd);				
-			}
-
-			for (MethodDeclaration md: methods){
-				countVars(md);
-			}			
+		if (node instanceof TypeDeclaration) {
+			this.fields = 0;
+			this.methods = 0;
+		} else if (node instanceof MethodDeclaration) {
+			this.parameters = 0;
+			this.locals = 0;
 		}
 	}
-	
-	private void countVars(MethodDeclaration md) throws Exception {
-		if (md.getBody() != null && md.getBody().getLocalVariable() != null){
-			List<ParameterDeclaration> params = md.getParameters();
-			for (ParameterDeclaration pd : params){
-				pd.setIndex(locals);
-				locals++;
-			}
-			locals = 0;
-			
-			List<LocalVariableDeclaration> localvars= md.getBody().getLocalVariable();
-			for (LocalVariableDeclaration lvd : localvars){
-				lvd.setIndex(locals);
-				locals++;
-			}
-		}
-		locals = 0;
-	}
-
-
-	private void countField(FieldDeclaration fd) throws Exception {
-		Modifiers md = fd.getModifiers();
-		if (md.containModifier(Modifier.STATIC)){
-			fd.setIndex(locals);
-			locals++;
-		}
-		locals = 0;
-		
-	}
-
 
 	@Override
 	public boolean visit(ASTNode node){
-		
+		if (node instanceof LocalVariableDeclaration) {
+			((LocalVariableDeclaration) node).setIndex(this.locals++);
+		} else if (node instanceof ParameterDeclaration) {
+			((ParameterDeclaration) node).setIndex(this.parameters++);
+		} else if (node instanceof FieldDeclaration) {
+			((FieldDeclaration) node).setIndex(this.fields++);
+		} else if (node instanceof MethodDeclaration) {
+			// TODO: Type decl with super type need inherited super class's counter 
+			((MethodDeclaration) node).setIndex(this.methods++);
+		}
 		return true;
 	}
 	
 	@Override
-	public void didVisit(ASTNode node){
-		
+	public void didVisit(ASTNode node) throws Exception{
+		super.didVisit(node);
 	}
 
 }
