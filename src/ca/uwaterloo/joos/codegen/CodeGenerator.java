0393d0cc5e07f36e21a99551f42d8aaa5079b8a0
Matt Baker <mdbaker60@gmail.com>
2013-04-07 12:35:47 -0400

    merge

--- src/ca/uwaterloo/joos/codegen/CodeGenerator.java
+++ src/ca/uwaterloo/joos/codegen/CodeGenerator.java
@@ -197,6 +197,7 @@ public class CodeGenerator extends SemanticsVisitor {
 						// THIS is in eax : eax+(4*(fieldIndex + 1))
 						// this.texts.add("mov [eax + " + 4*fd.getIndex() +
 						// "], 0" );
+
 					}
 				}
 			}
